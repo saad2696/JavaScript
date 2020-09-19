4ae76fb2d8aa2061e54bf095fcb4e974b6bd5a2c
Saad Hmed <saad.ahmed2696@gmail.com>
2020-09-20 01:16:53 +0500

    memory hoistin & scope

--- functionsbasic/es.js
+++ functionsbasic/es.js
@@ -7,3 +7,25 @@ function MakeCoffee(sugar,milk){
     return instruction;
 } 
 console.log(MakeCoffee(2,30));
+
+
+//embeded functions 
+function name(){
+ var fullname = "Saad Ahmed"
+ function  concat(name){
+var conName = "Mr. "+name 
+return conName
+ }
+ return concat(fullname)
+}
+
+console.log(name());
+
+//callable embeded functions //call functions with-in a function 
+
+function car(name){
+    return name();
+}
+console.log(car(function(){
+    return "Audi"
+}))
